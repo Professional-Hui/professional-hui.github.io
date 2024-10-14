4dbf72c77002482d942d982135f06ece2486b751
Sireer <sireerw@gmail.com>
2024-10-14 22:57:38 +0800

    change next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  siteUrl: 'reactresume.com',
+  siteUrl: 'https://professional-hui.github.io',
   exclude: ['/404*', '/500*'],
   transform: async (config, path) => {
     return {
